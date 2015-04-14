60a8f0506475d710102ef74218159b6e40d978aa
Andrey Kurdyumov <kant2002@gmail.com>
2015-04-14 22:34:03 +0600

    Add broadcasting of the message to the connected clients

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var WebSocketServer = require('ws').Server,
 wss.on('connection', function (ws) {
     ws.on('message', function (message) {
         console.log('received: %s', message);
+        wss.clients.forEach(function each(client) {
+            // Send message back to the 
+            // all connected clients
+            console.log("Send " + message + " to client");
+            client.send(message);
+        });
+
     });
 
     ws.send('Client connected to the server.');
