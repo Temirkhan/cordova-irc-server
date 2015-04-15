d460ee5794d36eeccba2fe21eab3b11cfaa35698
Andrey Kurdyumov <kant2002@gmail.com>
2015-04-15 10:40:13 +0600

    Add ability to specify port using command line arguments

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 /*jslint node:true*/
 'use strict';
-var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({ port: 8080 });
 
+// Read port as the first parameters of the command line.
+var WebSocketServer = require('ws').Server,
+    argv = require('yargs')
+        .usage('Usage: $0 -port [num]')
+        .default('port', 8080, 'Port on which server would be listening')
+        .argv,
+    wss;
+
+var port = argv.port || 8080;
+wss = new WebSocketServer({ port: port });
 wss.on('connection', function (ws) {
     ws.on('message', function (message) {
-        console.log('received: %s', message);
+        console.log('< %s', message);
         wss.clients.forEach(function each(client) {
             // Send message back to the 
             // all connected clients
-            console.log("Send " + message + " to client");
+            console.log("> %s to client", message);
             client.send(message);
         });
 
@@ -17,4 +25,4 @@ wss.on('connection', function (ws) {
 
     ws.send('Client connected to the server.');
 });
-console.log('IRC server started.');
+console.log('IRC server started on port ' + port + '.');
