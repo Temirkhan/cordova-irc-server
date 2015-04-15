3f5d1cc70ba8819d2f6c5d2ba1c9f3eed4e1c2db
Andrey Kurdyumov <kant2002@gmail.com>
2015-04-15 13:56:34 +0600

    Use Http server to establish connections

--- app.js
+++ app.js
@@ -2,16 +2,34 @@
 'use strict';
 
 // Read port as the first parameters of the command line.
-var WebSocketServer = require('ws').Server,
+var defaultPort = 8080,
+    WebSocketServer = require('ws').Server,
+    app = require('http').createServer(handler),
+    //io = require('socket.io')(app),
+    fs = require('fs'),
     argv = require('yargs')
         .usage('Usage: $0 -port [num]')
-        .default('port', 8080, 'Port on which server would be listening')
+        .default('port', process.env.PORT || defaultPort, 'Port on which server would be listening')
         .argv,
     wss;
 
-var port = argv.port || 8080;
-wss = new WebSocketServer({ port: port });
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+        
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+var port = argv.port;
+wss = new WebSocketServer({ server: app });
 wss.on('connection', function (ws) {
+    console.log('Client connected');
     ws.on('message', function (message) {
         console.log('< %s', message);
         wss.clients.forEach(function each(client) {
@@ -26,3 +44,4 @@ wss.on('connection', function (ws) {
     ws.send('Client connected to the server.');
 });
 console.log('IRC server started on port ' + port + '.');
+app.listen(port);
